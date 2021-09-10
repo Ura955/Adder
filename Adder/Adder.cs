de1869effdf972f80ce487c714ce146cdb49c305
GEM <haryutkin98@mail.ru>
2021-09-10 23:25:19 +0800

    Add main class

---
+++ Adder/Adder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Adder
+{
+    class Adder
+    {
+        static void Main(string[] args)
+        {
+            //Переменная суммы
+            int sum = 0;
+
+            //Цикл суммирования аргументов
+            for (int i = 0; i < args.Length; i++)
+            {
+                //Временная переменная, для преобразования аргумента из типа string в int
+                int buf;
+
+                //Преобразование аргумента
+                buf = System.Convert.ToInt32(args[i]);
+
+                //Суммирование
+                sum += buf;
+            }
+
+            //Вывод результата и ожидание нажатия любой клавиши для завершения программы
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
